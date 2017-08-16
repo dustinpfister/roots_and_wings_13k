bc10824f88239028493bc35cf2626ea0f9722ee6
dustinpfister <dustin.pfister@gmail.com>
2017-08-16 10:54:06 -0400

    world.js file added

---
+++ js/world.js
@@ -0,0 +1,24 @@
+var World = (function () {
+
+    var api = {
+
+        // create a new World
+        new : function () {
+
+            console.log('world.js: new world');
+
+        },
+
+        // update the world
+        update : function () {
+
+            console.log('world.js: tick');
+
+        }
+
+    };
+
+    return api;
+
+}
+    ());
