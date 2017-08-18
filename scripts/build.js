808a7f2cbaa2457135e906ce4e78ecfe40111d3e
dustinpfister <dustin.pfister@gmail.com>
2017-08-18 12:42:16 -0400

    build tool

---
+++ scripts/build.js
@@ -0,0 +1,50 @@
+var jsmin = require('jsmin').jsmin,
+fs = require('fs');
+
+var files = [
+
+    'world',
+    'canvas',
+    'main'
+],
+buildFile = '',
+i = 0;
+
+var build = function () {
+
+    if (i < files.length) {
+
+        fs.readFile('../js/' + files[i] + '.js', 'utf8', function (e, data) {
+
+            buildFile += jsmin(data);
+
+            i += 1;
+
+            build();
+
+        });
+
+    } else {
+
+        console.log(buildFile);
+
+        fs.writeFile('../build.js', buildFile, 'utf8', function (e) {
+
+            if (e) {
+
+                console.log('error');
+                console.log(e);
+
+            } else {
+
+                console.log('build file done');
+
+            }
+
+        });
+
+    }
+
+};
+
+build();
